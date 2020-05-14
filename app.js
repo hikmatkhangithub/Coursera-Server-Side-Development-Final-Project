f4b60959351309b513ae780245605a448eb627ce
hikmatkhanse <hikmathanse@gmail.com>
2020-05-14 17:34:33 +0200

    HTTPS openssl

--- app.js
+++ app.js
@@ -91,5 +91,14 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    res.redirect(
+      307,
+      "https://" + req.hostname + ":" + app.get("secPort") + req.url
+    );
+  }
+});
 module.exports = app;
