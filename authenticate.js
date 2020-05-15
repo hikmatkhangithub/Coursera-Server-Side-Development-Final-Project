1e95ceeff12408e88eb73e0ec31f05f6038232b5
hikmatkhanse <hikmathanse@gmail.com>
2020-05-16 01:24:30 +0200

    Passport Facebook OAuth authentication

--- authenticate.js
+++ authenticate.js
@@ -9,6 +9,36 @@ var ExtractJwt = require("passport-jwt").ExtractJwt;
 var jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
 var config = require("./config.js");
 
+var FacebookTokenStrategy = require("passport-facebook-token");
+
+exports.facebookPassport = passport.use(
+  new FacebookTokenStrategy(
+    {
+      clientID: config.facebook.clientId,
+      clientSecret: config.facebook.clientSecret,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ facebookId: profile.id }, (err, user) => {
+        if (err) {
+          return done(err, false);
+        }
+        if (!err && user !== null) {
+          return done(null, user);
+        } else {
+          user = new User({ username: profile.displayName });
+          user.facebookId = profile.id;
+          user.firstname = profile.name.givenName;
+          user.lastname = profile.name.familyName;
+          user.save((err, user) => {
+            if (err) return done(err, false);
+            else return done(null, user);
+          });
+        }
+      });
+    }
+  )
+);
+
 exports.getToken = function (user) {
   return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
 };
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/confusion",
+  facebook: {
+    clientId: "2622288101428277",
+    clientSecret: "7ff43282943aa05ae6242cfbccab381d",
+  },
 };
